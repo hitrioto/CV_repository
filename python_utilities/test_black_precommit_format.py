ee0e9706d362b1ed8f9c89b368706b60113c9fc5
hitrioto <hitrioto@abv.bg>
2024-05-02 10:25:35 +0300

    test again precommit isort

--- python_utilities/test_black_precommit_format.py
+++ python_utilities/test_black_precommit_format.py
@@ -1,11 +1,9 @@
-import numpy as np
-import pandas as pd 
-
-
 import os
 
-example_2d_matrxi = np.array([1,2,3],[1,2,3],)
+import numpy as np
+import pandas as pd
 
-
-
-
+example_2d_matrxi = np.array(
+    [1, 2, 3],
+    [1, 2, 3],
+)
